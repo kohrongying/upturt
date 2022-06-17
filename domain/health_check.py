e6f9c10b7743c5ba3639715cb90e938581156117
rongying <kohrongying@gmail.com>
2022-06-18 00:13:14 +0800

    [rongying] Add converter to convert healthcheck to airtable model.

--- domain/health_check.py
+++ domain/health_check.py
@@ -7,4 +7,12 @@ from domain.website_status import WebsiteStatus
 class HealthCheck(BaseModel):
     url: HttpUrl
     status: WebsiteStatus
-    status_dt: datetime = datetime.now()
+    status_dt: datetime = datetime.now().astimezone()
+
+    def to_airtable_model(self):
+        return {
+            "Domain": self.url,
+            "Status": "true" if self.status == WebsiteStatus.up else "false",
+            "Date": str(self.status_dt)
+        }
+
--- test/domain/test_health_check.py
+++ test/domain/test_health_check.py
@@ -11,3 +11,9 @@ class TestHealthCheck(unittest.TestCase):
     def test_health_check(self):
         hc = HealthCheck(url=parse_obj_as(HttpUrl, "http://google.com"), status=WebsiteStatus.up)
         self.assertIsNotNone(hc.status_dt)
+
+    def test_to_airtable_model(self):
+        hc = HealthCheck(url=parse_obj_as(HttpUrl, "http://google.com"), status=WebsiteStatus.up)
+        dict = hc.to_airtable_model()
+        self.assertEqual(dict["Domain"], "http://google.com")
+        self.assertEqual(dict["Status"], "true")
