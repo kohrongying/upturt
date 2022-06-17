8e697f3998c287e518c29d7f10650482456d8ce8
rongying <kohrongying@gmail.com>
2022-06-18 00:51:33 +0800

    [rongying] Print exception.

--- service/airtable_service.py
+++ service/airtable_service.py
@@ -15,9 +15,8 @@ status_table = Table(api_key, base_id, status_table_name)
 class AirtableService:
 
     @validate_arguments
-    def batch_create_health_check(self, health_check_records: List[HealthCheck]) -> None:
+    def batch_create_health_check(self, health_check_records: List[HealthCheck]):
         try:
             response = status_table.batch_create([hc.to_airtable_model() for hc in health_check_records])
-            print(response)
-        except Exception:
-            return None
+        except Exception as e:
+            print(e)
