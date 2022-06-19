cc67276779c65a82f54d86c8fc45a32d712747dd
rongying <kohrongying@gmail.com>
2022-06-19 18:43:19 +0800

    [rongying] Add basic templating to render the status on html. Add tailwind for basic styling.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from typing import Union, List
+from typing import  List
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+from fastapi.encoders import jsonable_encoder
 from domain.health_check import HealthCheck
 from service.ui_service import UiService
 
@@ -14,12 +14,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse, response_model=HealthCheck)
 async def read_root(request: Request):
     data: List[HealthCheck] = UiService.get_most_recent_records()
-    return templates.TemplateResponse("index.html", {"request": request, "data": data})
-
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "data": [jsonable_encoder(hc) for hc in data] }
+    )
