e42c8f393f85bfb2e866896b52638a8f8d621933
rongying <kohrongying@gmail.com>
2022-06-19 17:40:28 +0800

    [rongying] Convert airtable status record to healthcheck.

--- service/airtable_service.py
+++ service/airtable_service.py
@@ -1,11 +1,14 @@
 from typing import List, Dict
 
+import pytz
 from pyairtable import Table
 from pyairtable.formulas import FIELD, EQUAL, to_airtable_value, match
 
 from domain.health_check import HealthCheck
 from pydantic import validate_arguments, BaseModel
 import os
+from datetime import datetime
+from domain.website_status import WebsiteStatus
 
 api_key = os.environ.get("AIRTABLE_API_KEY")
 base_id = 'apph8KjTUqc4kP4wj'
@@ -17,6 +20,18 @@ class AirtableStatusRecord(BaseModel):
     id: str
     createdTime: str
     fields: Dict[str, str]
+
+    def to_health_check(self):
+        website_status = WebsiteStatus.up if self.fields.get('Status', False) else WebsiteStatus.down
+        status_dt_str = self.fields.get('Date', '')
+        utc_dt = datetime.strptime(status_dt_str, '%Y-%m-%dT%H:%M:%S.%f%z')
+        local_tz = pytz.timezone('Asia/Singapore')
+        local_dt = utc_dt.replace(tzinfo=pytz.utc)
+        return HealthCheck(
+            url=self.fields.get('Domain'),
+            status=website_status,
+            status_dt=local_dt.astimezone(local_tz)
+        )
 
 
 class AirtableService:
@@ -64,7 +79,6 @@ class AirtableService:
     @staticmethod
     def filter(formula: str, max_records: int, sort: List[str]) -> List[AirtableStatusRecord]:
         try:
-            print(formula)
             response = status_table.all(formula=formula, max_records=max_records, sort=sort)
             return [AirtableStatusRecord.parse_obj(r) for r in response]
         except Exception as e:
--- test/service/test_airtable_service.py
+++ test/service/test_airtable_service.py
@@ -1,10 +1,10 @@
 import unittest
 
-from pydantic import ValidationError, HttpUrl, parse_obj_as
+from datetime import datetime, timezone, date
+import pytz
 
 from domain.website_status import WebsiteStatus
 from service.airtable_service import AirtableStatusRecord
-from service.health_check_service import HealthCheckService
 
 
 class TestAirtableService(unittest.TestCase):
@@ -15,3 +15,17 @@ class TestAirtableService(unittest.TestCase):
         self.assertEqual(r.id, 'recJ5MjtVZL0X7b2l')
         self.assertEqual(r.createdTime, '2022-06-19T08:39:25.000Z')
         self.assertEqual(r.fields, {'Domain': 'https://rongying.co', 'Date': '2022-06-19T08:39:20.254Z', 'Created': '2022-06-19T08:39:25.000Z'})
+
+    def test_airtable_status_record_to_health_check(self):
+        airtable_response = {'id': 'recJ5MjtVZL0X7b2l', 'createdTime': '2022-06-19T08:39:25.000Z',
+                             'fields': {'Domain': 'https://rongying.co', 'Date': '2022-06-19T08:39:20.254Z',
+                                        'Created': '2022-06-19T08:39:25.000Z'}}
+        r = AirtableStatusRecord.parse_obj(airtable_response)
+        hc = r.to_health_check()
+        self.assertEqual(hc.url, 'https://rongying.co')
+        self.assertEqual(hc.status, WebsiteStatus.down)
+        local_tz = pytz.timezone('Asia/Singapore')
+
+        self.assertEqual(hc.status_dt.date(), date(2022, 6, 19))
+        self.assertEqual(hc.status_dt.hour, 16)
+        self.assertEqual(hc.status_dt.minute, 39)
